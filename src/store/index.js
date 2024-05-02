56b4835d52314a1a7b6538eb20fb9826852c4da8
Hari Shankar R <harishankar990326@gmail.com>
2024-05-02 15:11:08 +0530

    #Store updation

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,3 @@
-// // store.js
-// import { createStore, compose, applyMiddleware } from 'redux';
-// import ListReducer from '../reducer';
-// import { ConversationData } from '../mock-response';
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(ListReducer, {coversations : ConversationData, selectedId : 1},composeEnhancers(applyMiddleware()));
-
-// export default store;
-
 import { configureStore } from '@reduxjs/toolkit';
 import ListReducer from '../reducer';
 
